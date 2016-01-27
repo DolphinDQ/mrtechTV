36ce1ae7fa04e2bb5b5bff150acffdbb41cb8e58
sphynx <229860255@qq.com>
2016-01-27 08:40:57 +0800

    集成Bugly SDK

--- app/src/main/java/mrtech/tv/App.java
+++ app/src/main/java/mrtech/tv/App.java
@@ -1,12 +1,10 @@
 package mrtech.tv;
 
-import android.os.Handler;
-import android.widget.Toast;
+import com.tencent.bugly.crashreport.CrashReport;
 
 import mrtech.smarthome.SmartHomeApp;
 import mrtech.smarthome.router.Router;
 import mrtech.smarthome.router.RouterManager;
-import rx.functions.Action1;
 
 /**
  * Created by sphynx on 2016/1/12.
@@ -15,6 +13,7 @@ public class App extends SmartHomeApp {
     @Override
     public void onCreate() {
         super.onCreate();
+        CrashReport.initCrashReport(this, "900018908", false);
         RouterManager.getInstance().addRouter(new Router("router","6YP84F-50XKQ2-DX2V4T-BZ8L6C-UAX9NY-9D2"));
     }
 }
